51703bb59435407c944a325a4f17d52ba713da7b
Viktoriia <vis22chu@gmail.com>
2020-10-01 22:42:21 +0400

    feat: add function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members) || members.length === 0) return false;
+  members = members.filter(item => typeof item === "string");
+  return members.map(i => i.trim().toUpperCase().charAt(0)).sort().join('');
 };
